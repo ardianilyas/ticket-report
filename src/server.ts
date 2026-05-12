698c463f42583a2355c252986bcb8274c94e5535
ardianilyas <ardianilyas@gmail.com>
2026-05-12 07:56:46 +0700

    chore: remove test route

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import express from "express";
 import { auth } from "./lib/auth";
 import { errorHandler } from "./middlewares/error-handler";
 import { env } from "./config/env";
-import z from "zod";
-import { validate } from "./shared/utils/validate";
 
 const app = express();
 const PORT = env.PORT;
@@ -12,20 +10,6 @@ const PORT = env.PORT;
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
-
-const userSchema = z.object({
-  username: z.string(),
-  password: z.string(),
-});
-
-app.post("/api/users", async (req, res, next) => {
-  try {
-    const data = validate(userSchema, req.body);
-    res.json(data);
-  } catch (error) {
-    next(error);
-  }
-});
 
 app.use(errorHandler);
 
