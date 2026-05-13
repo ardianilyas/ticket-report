c8e341eff7916c6f444da9e37d11d3a0f6be645f
ardianilyas <ardianilyas@gmail.com>
2026-05-13 15:46:38 +0700

    feat: update db config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,16 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
+import { env } from "../config/env";
+import * as schemas from "../db/schemas";
+import * as relations from "../db/relations";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new pg.Pool({
+  connectionString: env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, {
+  schema: {
+    ...schemas,
+    ...relations
+  }
+});
