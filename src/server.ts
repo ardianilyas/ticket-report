0042f451010656492c10ec356aa82e80822c154b
ardianilyas <ardianilyas@gmail.com>
2026-05-23 23:18:23 +0700

    feat: add CORS middleware pointing to port 5173

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import cors from "cors";
 import { auth } from "./lib/auth";
 import { errorHandler } from "./middlewares/error-handler";
 import { env } from "./config/env";
@@ -7,6 +8,13 @@ import apiRoute from "./routes";
 
 const app = express();
 const PORT = env.PORT;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
